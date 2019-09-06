078633e2d5727d11965c56dcd60dd3c0b857fa54
acipcong <garuda56@gmail.com>
2019-09-06 07:29:26 +0530

    project creation

---
+++ app/src/main/java/tiles/prokarma/co/tiles/views/activities/HomeActivity.java
@@ -0,0 +1,15 @@
+package tiles.prokarma.co.tiles.views.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import tiles.prokarma.co.tiles.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
